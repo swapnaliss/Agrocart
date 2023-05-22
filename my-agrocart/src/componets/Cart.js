6570b0d3dff20984865d350719a4aa813518560a
swapnaliss <swapnalisbhargude123@gmail.com>
2023-05-22 14:08:48 +0530

    feat:basic UI done for cart page

--- my-agrocart/src/componets/Cart.js
+++ my-agrocart/src/componets/Cart.js
@@ -1,9 +1,75 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Table, Button } from 'react-bootstrap';
 
-const Cart = () => {
+const CartPage = () => {
+  const cartItems = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 10,
+      quantity: 2,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 15,
+      quantity: 1,
+    },
+    {
+      id: 2,
+      name: 'Product 3',
+      price: 15,
+      quantity: 1,
+    },
+  ];
+
   return (
-    <div>Cart</div>
-  )
-}
+    <Container>
+      <Row className="mt-4">
+        <Col>
+          <h2>Cart</h2>
+          {cartItems.length > 0 ? (
+            <Table striped bordered responsive>
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Price</th>
+                  <th>Quantity</th>
+                  <th>Total</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.name}</td>
+                    <td>${item.price}</td>
+                    <td>{item.quantity}</td>
+                    <td>${item.price * item.quantity}</td>
+                    <td>
+                      <Button variant="danger">Remove</Button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan="3" className="text-right">Total:</td>
+                  <td>${cartItems.reduce((total, item) => total + item.price * item.quantity, 0)}</td>
+                  <td></td>
+                </tr>
+              </tfoot>
+            </Table>
+          ) : (
+            <p>Your cart is empty.</p>
+          )}
 
-export default Cart
+          {/* Continue Shopping Button */}
+          <Button variant="primary">Continue Shopping</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default CartPage;
