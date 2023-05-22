03e790c0b1b11bc7e43ddbcea36d9f881523900d
swapnaliss <swapnalisbhargude123@gmail.com>
2023-05-22 14:34:46 +0530

    feat: basic UI done for wishlist page

--- my-agrocart/src/componets/Wishlist.js
+++ my-agrocart/src/componets/Wishlist.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
-const Wishlist = () => {
+const WishlistPage = () => {
+  const wishlistItems = [
+    {
+      id: 1,
+      name: 'Product 1',
+      price: 10,
+      image: 'image1',
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      price: 15,
+      image: 'image2',
+    },
+    
+  ];
+
   return (
-    <div>Wishlist</div>
-  )
-}
+    <Container>
+      <Row className="mt-4">
+        <Col>
+          <h2>Wishlist</h2>
+          {wishlistItems.length > 0 ? (
+            <Row>
+              {wishlistItems.map((item) => (
+                <Col sm={4} key={item.id} className="mb-4">
+                  <Card>
+                    <Card.Img variant="top" src={item.image} alt={item.name} />
+                    <Card.Body>
+                      <Card.Title>{item.name}</Card.Title>
+                      <Card.Text>Price: ${item.price}</Card.Text>
+                      <Button variant="primary">Move to Cart</Button>
+                      <span className="favorite-icon ml-3">&#9825;</span>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <p>Your wishlist is empty.</p>
+          )}
+          <Button variant="primary">Continue Shopping</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Wishlist
+export default WishlistPage;
