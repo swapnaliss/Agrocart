e9762231b4b8c60c145e2d56d0950eeef6c0091e
swapnaliss <swapnalisbhargude123@gmail.com>
2023-05-22 11:06:03 +0530

    feature : all the products are listed with a section of filters.

--- my-agrocart/src/componets/ProductListingPage.js
+++ my-agrocart/src/componets/ProductListingPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const ProductListingPage = () => {
   return (
@@ -13,7 +13,20 @@ const ProductListingPage = () => {
         {/* Product Listing Section */}
         <Col sm={9}>
           <Row>
-           product section
+           {/* Product Cards */}
+           {[...Array(20)].map((_, index) => (
+              <Col sm={4} key={index} className="mb-4">
+                <Card>
+                  <Card.Img variant="top" src="https://example.com/product-image.jpg" alt="Product" />
+                  <Card.Body>
+                    <Card.Title>Product Name</Card.Title>
+                    <Card.Text>Price: $10</Card.Text>
+                    <Button variant="primary">Add to Cart</Button>
+                    <span className="favorite-icon">&#9825;</span>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
           </Row>
         </Col>
       </Row>
